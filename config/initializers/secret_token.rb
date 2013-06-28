43e6035b8007e4d78effa641dbbb130e62f4c6d0
MakerSquare Student <student@lab-3.att.net>
2013-06-28 14:01:23 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RotationDiet::Application.config.secret_token = '17c2c0e91e6abcf4e75cd4808b7cf6a214a1bc9c08cc1edbd24a9b48c7c54a002a81e279f357c1aa980cd2d73453a1f2f19cc66bee7cbdfde024941d5209cdc8'
